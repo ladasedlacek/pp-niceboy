f0ceded9bd0dc9e7342ec6c00046f0c9ab266ac5
ladasedlacek <sedlacek.lada@gmail.com>
2022-03-05 23:46:25 +0100

    Final version

---
+++ README.md
@@ -0,0 +1,2 @@
+# pp-henkel
+PP Henkel
---
+++ js/script.js
@@ -0,0 +1,19 @@
+/* jshint esversion:6 */
+$(document).ready(function () {
+	// Toggle layouts
+	$("#layout_switch button").on("click", function () {
+		$("#body2").removeClass("layout-0").removeClass("layout-1");
+		$("body").addClass("mobile");
+	});
+	$("#layout_switch button:nth-child(2)").on("click", function () {
+		$("#body2").addClass("layout-0");
+		$("body.mobile").removeClass("mobile");
+	});
+	$("#layout_switch button:nth-child(3)").on("click", function () {
+		$("#body2").addClass("layout-1");
+		$("body.mobile").removeClass("mobile");
+	});
+
+	// if admin is active
+	-1 < window.location.href.indexOf("admin") ? 0 : runSwiper()
+});
